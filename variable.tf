694bd28f5976cf19a20cc78f8128a28f25da8dcf
amananp <aman.a.shah@capgemini.com>
2017-02-01 16:31:51 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,26 @@
+variable "region" {
+        default = "us-west-2"
+}
+variable "availability_zone" {
+        default = "us-west-2a"
+}
+
+variable "instance_type" {
+        default = "t2.micro"
+}
+
+variable "aws_ami" {
+        default = "ami-30e65350"
+}
+
+variable "key_name" {
+        default = "aman"
+}
+
+variable "aws_access_key" {
+        default = "AKIAJPBLJKATBHML254A"
+}
+
+variable "aws_secret_key" {
+        default = "oBL085SMWS56H0Nq10gOS931lAjgN1H0+sYIUaZ7"
+}
